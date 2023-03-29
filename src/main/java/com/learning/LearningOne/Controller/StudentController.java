fe714c9e02f642b8949ce31aeade1639673cd7de
vgrgala@gmail.com <Chinna@1025>
2023-03-29 00:34:06 -0400

    Added changes for javax persistence

--- src/main/java/com/learning/LearningOne/Controller/StudentController.java
+++ src/main/java/com/learning/LearningOne/Controller/StudentController.java
@@ -3,17 +3,19 @@ package com.learning.LearningOne.Controller;
 import com.learning.LearningOne.Model.Student;
 import com.learning.LearningOne.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "api/v1/student")
+@RequestMapping("api/v1/student")
 public class StudentController {
 
     @Autowired
-    private StudentService studentService;
+    StudentService studentService;
 
     @GetMapping
     public List<Student> getStudents() {
@@ -21,8 +23,9 @@ public class StudentController {
     }
 
     @PostMapping
-    public void saveStudent(@RequestBody Student student) throws Exception {
+    public ResponseEntity<String> saveStudent(@RequestBody Student student){
          studentService.saveStudent(student);
+         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
@@ -31,7 +34,7 @@ public class StudentController {
     }
 
     @PatchMapping("/{id}")
-    public void updateStudent(@RequestBody Student student, @PathVariable Long id) throws Exception {
+    public void updateStudent(@RequestBody Student student, @PathVariable Long id) {
         studentService.updateStudent(student,id);
     }
 
